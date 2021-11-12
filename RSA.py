45f5085eb900529abe3cbd8be1008ddd4e2086b4
K3RM1T <contact@k3rm1t.fr>
2021-11-12 16:32:45 +0100

    Added key generation function

--- RSA.py
+++ RSA.py
@@ -3,13 +3,29 @@ from Crypto.Cipher import PKCS1_v1_5
 import base64
 
 
+def generatekeys():
+    key = RSA.generate(2048)
+    pubKey = key.public_key().exportKey('PEM').decode()
+    publicKeyFile = open('publicKey.pem', 'w')
+    publicKeyFile.write(pubKey)
+    publicKeyFile.close()
+    print("CLE PUBLIQUE OK\n")
+
+    privKey = key.exportKey('PEM').decode()
+    privateKeyFile = open('privateKey.pem', 'w')
+    privateKeyFile.write(privKey)
+    privateKeyFile.close()
+    print("CLE PRIVÉE OK\n")
+
+    return 0
+
+
 def encryptdata(message):
     key_text = open(input("Entrez le nom du fichier contenant la clé publique du destinataire : "), "rb").read()
     public_key = RSA.importKey(key_text)
     cipher = PKCS1_v1_5.new(public_key)
     message = message.encode()
     encrypted_bytes = cipher.encrypt(message)
-
     return base64.b64encode(encrypted_bytes)
 
 
@@ -30,11 +46,11 @@ print("   2: Chiffrer un message")
 print("   3: Déchiffrer un message\n")
 resultChoice = int(input("Entrez votre choix : "))
 
-if resultChoice == 1:
+if resultChoice == 2:
     dataEncrypted = encryptdata(input("Entrez votre message à chiffrer : "))
     print(dataEncrypted.decode())
-if resultChoice == 2:
+if resultChoice == 3:
     dataDecrypted = decryptdata(input("Entrez le message à déchiffrer : "))
     print(dataDecrypted.decode())
-if resultChoice == 3:
-    print("En construction...")
+if resultChoice == 1:
+    generatekeys()
--- readme.md
+++ readme.md
@@ -1,5 +1,1 @@
-To use this program you must generate a private / public key pair and put them in the folder containing "RSA.py"
-
-The private key file must be named "privateKey.pem"
-
-The public key file must be named "publicKey.pem"
+This program enables message encryption with the AES encryption method.
